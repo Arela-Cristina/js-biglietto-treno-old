be8fded3fda7888392ce7b08acbb3e93fa7a895c
Arela-Cristina <instagrandine@gmail.com>
2024-06-04 14:57:26 +0200

    Add variables
    let numberKm to know through a prompt how many kilometers the passenger want to travel
    
    let priceKm to specificate the passenger how much cot each kilometer
    
    let travelPrice to inform the passenger how much cost the total, respect to the kilometers which want to travel.

--- main.js
+++ main.js
@@ -1,2 +1,11 @@
-let numberKm = prompt('How many kikometers do you wan to travel?')
-console.log(numberKm)
+// number of Km 
+let numberKm = prompt('How many kilometers do you wan to travel?');
+console.log("Km che desidera viaggiare", numberKm);
+
+// price of each kilometer
+let priceKm = 0.21;
+console.log("prezzo di ogni Km percorso", priceKm);
+
+// price of the total kilometers traveled
+let travelPrice = numberKm * priceKm;
+console.log("Prezzo per il totale del viaggio", travelPrice);
