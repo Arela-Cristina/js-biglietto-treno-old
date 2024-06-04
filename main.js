87fe452cd86fe4fbedbf57cefe5d87388a807767
Arela-Cristina <instagrandine@gmail.com>
2024-06-04 21:45:45 +0200

    add  document.getElementById to some variables

--- main.js
+++ main.js
@@ -1,10 +1,12 @@
 // number of Km 
 let numberKm = prompt('How many kilometers do you wan to travel?');
 console.log("Km che desidera viaggiare", numberKm);
+document.getElementById("travelkm").innerHTML = numberKm;
 
 // // Passenger Age
 let passengerAge = prompt('You can get a discount, respect of your age. How old are you?');
 console.log("La tua eta", passengerAge);
+document.getElementById("age").innerHTML = passengerAge;
 
 // price of each kilometer
 let priceKm = 0.21;
@@ -13,18 +15,20 @@ console.log("prezzo di ogni Km percorso", priceKm);
 // price of the total kilometers traveled
 let travelPrice = numberKm * priceKm;
 console.log("Prezzo per il totale del viaggio", travelPrice);
+document.getElementById("totale").innerHTML = travelPrice;
 
 // discount if your are minor
 if (passengerAge < 18) {
     let percentage = (travelPrice * 20) / 100;
-    let discountForMinors = travelPrice - percentage;
-    console.log("prezzo scontato per over 65", discountForMinors.toFixed(2) + '€');
-}
+    let discountForMinors = (travelPrice - percentage).toFixed(2);
+    console.log("prezzo scontato per over 65", discountForMinors + '€');
+    document.getElementById("sconto-minori").innerHTML = discountForMinors;
 
-// discount if your are Older Adult
-if (passengerAge > 65) {
+    // discount if your are Older Adult
+} if (passengerAge > 65) {
     let percentage = (travelPrice * 40) / 100;
-    let discountForOlderAdult = travelPrice - percentage;
-    console.log("prezzo scontato per over 65", discountForOlderAdult.toFixed(2) + '€');
+    let discountForOlderAdult = (travelPrice - percentage).toFixed(2);
+    console.log("prezzo scontato per over 65", discountForOlderAdult + '€');
+    document.getElementById("sconto-over60").innerHTML = discountForOlderAdult;
 }
 
